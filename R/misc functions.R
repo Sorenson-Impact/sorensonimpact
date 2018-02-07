7c5e1503cb1de4144f846863002ab4fb6a1bec26
Jon Zadra <jon.zadra@sorensonimpact.com>
2018-02-07 14:18:35 -0700

    updated roxygen for %ni%

--- R/misc functions.R
+++ R/misc functions.R
@@ -6,8 +6,12 @@ wc <- function(x = .Last.value) {
   message("Value copied to clipboard")
 }
 
-#' Value not matching (oppsoite of %in%)
-#' @description Returns a logical vector indicating where there is not a match for its left operation.  Opposite of %in%
-#' @return A logical vector, indicating if there was no match for each element.
+#' Not In: Inverse Value Matching
+#' @description Returns a logical vector indicating if there is NOT a match for the LHS vector anywhere in the RHS vector. Opposide of \code{\%in\%}.
+#' @usage x %ni% y
+#' @param x vector or NULL: the values to check for non-match
+#' @param y vector or NULL: the values to be matched against
+#' @return A logical vector, indicating if there was no match for each element of x. Values are TRUE or FALSE and never NA
+#' @rdname ni
 #' @export
 "%ni%" <- Negate("%in%")
