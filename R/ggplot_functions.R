44f7ba81267858f318e584980ff43f549fff24fb
Jon Zadra <jon.zadra@sorensonimpact.com>
2018-02-07 14:18:18 -0700

    changed default for add_logo to TRUE

--- R/ggplot_functions.R
+++ R/ggplot_functions.R
@@ -8,7 +8,7 @@
 #' @param width Width in inches (default: 6).
 #' @param height Height in inches (default: 4).
 #' @param dpi Dots per inch. Defaults to 300.  The resolution of the file will be width*DPI by height*DPI.
-#' @param add_logo Logical. Add the sorenson impact branding bar?
+#' @param add_logo Logical. Add the sorenson impact branding bar? (defaults: TRUE)
 #' @param logo_height_ratio Number between 0 and 1, with sensibile values between .01 and .1.  The percent of the image height that the bar should be. Default is 0.05.
 #' @param band_color The color of the SI Logo band.  Defaults to \code{si_design$granite}.
 #' @return A png of the last plot with optional SI logo band.
@@ -16,7 +16,7 @@
 #' SI_colorplot() + ggplot2::ggtitle("My Title")
 #' SI_ggsave(add_logo = TRUE)
 #' @export
-si_ggsave <- function(filename = "auto", dir = "auto", plot = ggplot2::last_plot(), width = 6, height = 4, dpi = 300, add_logo = FALSE, logo_height_ratio = .05, band_color = si_design$granite) {
+si_ggsave <- function(filename = "auto", dir = "auto", plot = ggplot2::last_plot(), width = 6, height = 4, dpi = 300, add_logo = TRUE, logo_height_ratio = .05, band_color = si_design$granite) {
 
 
   if(dir == "auto"){
@@ -106,4 +106,44 @@ si_ggplot_theme_update <- function() {
                             strip.text = ggplot2::element_text(family = "Roboto"))
     }
   } else warning("Package extrafont not installed or Roboto font family not installed.")
+}
+
+
+#' Scale format to International System of Units (k, M, )
+#' @description Format a vector of numeric values according to the International System of Units.
+#'
+#' (ie: 1,000 becomes "1 k", 2,500 becomes "2.5 k") See: http://en.wikipedia.org/wiki/SI_prefix
+#' @param sep Seperator to use between number and unit (defaults to " ")
+#' @param ... Passed by \code{ggplot2::scale_*_continuous}
+#'
+#' @return Used with scale_*_continuous, returns formatted axis labels
+#' @export
+#'
+#' @examples
+#' ggplot(diamonds, aes(x = price)) + geom_density() + scale_x_continuous(labels = scale_si_unit())
+
+scale_si_unit <- function(sep = " ", ...) {
+  # Based on code by Ben Tupper
+  # https://stat.ethz.ch/pipermail/r-help/2012-January/299804.html
+
+    function(x) {
+    limits <- c(1e-24, 1e-21, 1e-18, 1e-15, 1e-12,
+                1e-9,  1e-6,  1e-3,  1e0,   1e3,
+                1e6,   1e9,   1e12,  1e15,  1e18,
+                1e21,  1e24)
+    prefix <- c("y",   "z",   "a",   "f",   "p",
+                "n",   "µ",   "m",   " ",   "k",
+                "M",   "G",   "T",   "P",   "E",
+                "Z",   "Y")
+
+    # Vector with array indices according to position in intervals
+    i <- findInterval(abs(x), limits)
+
+    # Set prefix to " " for very small values < 1e-24
+    i <- ifelse(i==0, which(limits == 1e0), i)
+
+    paste(format(round(x/limits[i], 1),
+                 trim=TRUE, scientific=FALSE, ...),
+          prefix[i], sep = sep)
+  }
 }
