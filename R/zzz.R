0b1408e9fe427c1f8ebf4051e7d59643a5301138
Jon Zadra <jon.zadra@sorensonimpact.com>
2018-02-28 14:34:02 -0700

    Added packages "fs" and "readxl" to autoload.
    Added commented version of autoupdate (work in progress)

--- R/zzz.R
+++ R/zzz.R
@@ -2,4 +2,42 @@
   library(tidyverse)
   library(knitr)
   library(rmarkdown)
+  library(fs)
+  library(readxl)
 }
+
+# check_si_up_to_date <- function(pkg, repo="github") {
+#   installed_version <- tryCatch(packageVersion(gsub(".*/", "", pkg)), error=function(e) NA)
+#
+#   if(repo == "github") {
+#     url <- paste0("https://raw.githubusercontent.com/", pkg, "/master/DESCRIPTION")
+#   } else if (repo == "gitlab") {
+#     url <- paste0("https://gitlab.com/", pkg, "/raw/master/DESCRIPTION")
+#   } else {
+#     stop("only work with github and gitlab")
+#   }
+#
+#   x <- readLines(url)
+#   remote_version <- gsub("Version:\\s*", "", x[grep('Version:', x)])
+#
+#   res <- list(package = pkg,
+#               installed_version = installed_version,
+#               latest_version = remote_version,
+#               up_to_date = NA)
+#
+#   if (is.na(installed_version)) {
+#     message(paste("##", pkg, "is not installed..."))
+#     message(msg)
+#   } else {
+#     if (remote_version > installed_version) {
+#       msg <- paste("##", pkg, "is out of date...")
+#       message(msg)
+#       res$up_to_date <- FALSE
+#     } else if (remote_version == installed_version) {
+#       message("package is up-to-date devel version")
+#       res$up_to_date <- TRUE
+#     }
+#   }
+#
+#   return(res)
+# }
