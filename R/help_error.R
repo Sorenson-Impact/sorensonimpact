766977b8d15e9c26f5ba19a934b1b70c1aa32609
Jon Zadra <jon.zadra@sorensonimpact.com>
2020-03-31 13:42:52 -0600

    export help_error_* and version up

--- R/help_error.R
+++ R/help_error.R
@@ -1,3 +1,8 @@
+#' Search Stack Overflow for help on errors
+#' @description Search the web for help on errors
+#' @importFrom magrittr "%>%"
+#' @export
+
 help_error_so <- function(error = geterrmessage(), add_error_term = T) {
   error_plus <- stringr::str_replace_all(geterrmessage(), pattern = " ", replacement = "+")
   if(add_error_term) error_plus <- paste("error", error_plus, sep = "+")
@@ -5,6 +10,10 @@ help_error_so <- function(error = geterrmessage(), add_error_term = T) {
   browseURL(url)
 }
 
+#' Search Google for help on errors
+#' @description Search the web for help on errors
+#' @importFrom magrittr "%>%"
+#' @export
 help_error_google <-  function(error = geterrmessage(), add_error_term = T) {
   error_plus <- stringr::str_replace_all(geterrmessage(), pattern = " ", replacement = "+")
   if(add_error_term) error_plus <- paste("error", error_plus, sep = "+")
