b34ac7028b6c6705c2fe475f81ccd3b0d0892848
Jon Zadra <jon.zadra@sorensonimpact.com>
2021-06-11 12:47:52 -0600

    add option for adding rownames

--- R/xview.R
+++ R/xview.R
@@ -27,13 +27,14 @@
 #' \lifecycle{experimental}
 #' Opens the table in a temporary file in excel.  Currently only works on Mac OS.
 #' @param .data A table.
+#' @param add_rownames Logical. Convert rownames to column? Default: TRUE
 #' @examples
 #' \dontrun{
 #' diamonds %>% xview()
 #' }
 #' @export
 
-xview <- function (.data) {
+xview <- function (.data, add_rownames = TRUE) {
   tempFilePath = paste(tempfile(), ".xlsx")
   tempPath = dirname(tempFilePath)
   preferredFile = paste(deparse(substitute(.data)), ".xlsx", sep = "")
@@ -53,7 +54,7 @@ xview <- function (.data) {
   #rownames(.data) = NULL
   #.data = data.frame(RowLabels = tmp, .data)
 
-  if(tibble::has_rownames(data)) .data <- .data %>% tibble::rownames_to_column()
+  if(add_rownames & tibble::has_rownames(data)) .data <- .data %>% tibble::rownames_to_column()
 
   WriteAttempt = try(
     WriteXLS::WriteXLS(as.character(bquote(.data)), ExcelFileName=preferredFilePath, FreezeRow=1, FreezeCol=1, BoldHeaderRow=T, AdjWidth=F, AutoFilter=T, row.names=F),
