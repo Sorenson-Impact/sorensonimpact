867128e84368a39271725e39104f54c28c7f34cb
Jon Zadra <jon.zadra@sorensonimpact.com>
2021-07-23 13:01:11 -0600

    formatting of major update info

--- R/misc functions.R
+++ R/misc functions.R
@@ -149,10 +149,9 @@ si_update <- function() {
       center = paste0("Major Update"),
       line_col = "red"
     ))
-    cli::cat_line()
     #cli::cli_alert_info("New features added. Run \`si_news()\` to view notes.")
     si_news()
-    cli::cat_line()
+    cli::cli_alert_info("Run \`si_news(all = T)\` to view previous update news.")
 
   } else {
     cli::cat_line()
